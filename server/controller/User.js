6a7c3b3df2020d951fcfd2091fa163c8f021fbc8
adarshakanel <52978035+adarshakanel@users.noreply.github.com>
2022-03-12 16:03:11 -0700

    cloudinary allowed

--- server/controller/User.js
+++ server/controller/User.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const router = express.Router();
-// recordRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /record.
-// const recordRoutes = express.Router();
 const User = require("../schema/User")
-// This will help us connect to the database
-// const dbo = require("../conn");
+const cloudinary = require('cloudinary').v2;
+require('dotenv').config();
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.COULDINARY_SECRET
+});
+
+// "tree-736885__480.webp"
+module.exports.uploadToColudinary = (fileAddress) => {
+    try {
+        cloudinary.uploader.upload(fileAddress,
+            { folder: '513-social-media-clone' },
+            function (error, result) { console.log(result) });
+    } catch (err) {
+        console.log(err)
+    }
+}
 
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
@@ -26,14 +38,6 @@ module.exports.getUser = async (req, res, next) => {
     const user = await User.findById(id)
     if (user) res.status(200).send(user)
     else res.status(400).send("user not found")
+    // the url sent back is that of the image
+    // this.uploadToColudinary("./tree-736885__480.webp")
 };
-
-// router.post("/create", async (req, res, next) => {
-//     this.postUser(req, res, next)
-// })
-
-// router.get("/:id", async (req, res, next) => {
-//     this.getUser(req, res, next)
-// })
-
-// module.exports = router;
