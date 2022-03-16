58bdbb8f688325afb9ee7734a473d1a66161e57d
PriyavartRajain <47432873+PriyavartRajain@users.noreply.github.com>
2022-03-15 19:26:08 -0600

    Created login and signup page

--- social-media-clone/src/Routes/Route.js
+++ social-media-clone/src/Routes/Route.js
@@ -1,5 +1,6 @@
 import Login from "../general/Login";
 import { Route, Routes } from 'react-router-dom';
+import SignUp from "../general/SignUp";
 import Home from "../general/Home";
 
 function AllRoutes() {
@@ -8,6 +9,7 @@ function AllRoutes() {
             <Routes>
                 <Route path={"/"} exact element={<Home />} />
                 <Route path={"/login"} exact element={<Login />} />
+                <Route path={"/signup"} exact element={<SignUp />} />
             </Routes>
         </>
     )
