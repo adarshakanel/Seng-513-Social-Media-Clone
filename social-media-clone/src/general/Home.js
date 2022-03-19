ab4ddc46f37b3aaafe32cbd58ef2358d6a029637
adarshakanel <52978035+adarshakanel@users.noreply.github.com>
2022-03-19 15:27:53 -0600

    change stlying

--- social-media-clone/src/general/Home.js
+++ social-media-clone/src/general/Home.js
@@ -3,75 +3,20 @@ import Posts from './Posts'
 import './../css/Home.css'
 
 export const Home = () => {
+    const posts = []
+    for (let i = 0; i < 20; i++) {
+        posts.push(<Posts />)
+    }
+
     return (
         <div className='background-div homepage-div'>
             <ul className='posts'>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
-                <li>{<Posts />}</li>
+                {
+                    posts.map(arg => <li>{arg}</li>)
+                }
             </ul>
         </div>
-    )}
+    )
+}
 
 export default Home;
