2b0147db01c41d83e093e62332324a8b9740c33b
PriyavartRajain <47432873+PriyavartRajain@users.noreply.github.com>
2022-03-18 23:48:23 -0600

    Implemented home

--- social-media-clone/src/general/Home.js
+++ social-media-clone/src/general/Home.js
@@ -3,7 +3,65 @@ import "../css/Home.css"
 
 export const Home = () => {
     return (
-        <div>Home</div>
+        <div className='background-div homepage-div'>
+            <ul className='posts'>
+                <li>Post1</li>
+                <li>Post2</li>
+                <li>Post3</li>
+                <li>Post4</li>
+                <li>Post1</li>
+                <li>Post2</li>
+                <li>Post3</li>
+                <li>Post4</li>
+                <li>Post1</li>
+                <li>Post2</li>
+                <li>Post3</li>
+                <li>Post4</li>
+                <li>Post1</li>
+                <li>Post2</li>
+                <li>Post3</li>
+                <li>Post4</li>
+                <li>Post1</li>
+                <li>Post2</li>
+                <li>Post3</li>
+                <li>Post4</li>
+                <li>Post1</li>
+                <li>Post2</li>
+                <li>Post3</li>
+                <li>Post4</li>
+                <li>Post1</li>
+                <li>Post2</li>
+                <li>Post3</li>
+                <li>Post4</li>
+                <li>Post1</li>
+                <li>Post2</li>
+                <li>Post3</li>
+                <li>Post4</li>
+                <li>Post1</li>
+                <li>Post2</li>
+                <li>Post3</li>
+                <li>Post4</li>
+                <li>Post1</li>
+                <li>Post2</li>
+                <li>Post3</li>
+                <li>Post4</li>
+                <li>Post1</li>
+                <li>Post2</li>
+                <li>Post3</li>
+                <li>Post4</li>
+                <li>Post1</li>
+                <li>Post2</li>
+                <li>Post3</li>
+                <li>Post4</li>
+                <li>Post1</li>
+                <li>Post2</li>
+                <li>Post3</li>
+                <li>Post4</li>
+                <li>Post1</li>
+                <li>Post2</li>
+                <li>Post3</li>
+            </ul>
+        </div>
     )
 }
 
