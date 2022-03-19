c4e96993a0a2e7075d68fa01b934869d773536f6
shehzan31 <47336141+shehzan31@users.noreply.github.com>
2022-03-19 15:04:19 -0600

    added posts to the home page

--- social-media-clone/src/general/Home.js
+++ social-media-clone/src/general/Home.js
@@ -6,61 +6,70 @@ export const Home = () => {
     return (
         <div className='background-div homepage-div'>
             <ul className='posts'>
-                <li>Post1</li>
-                <li>Post2</li>
-                <li>Post3</li>
-                <li>Post4</li>
-                <li>Post1</li>
-                <li>Post2</li>
-                <li>Post3</li>
-                <li>Post4</li>
-                <li>Post1</li>
-                <li>Post2</li>
-                <li>Post3</li>
-                <li>Post4</li>
-                <li>Post1</li>
-                <li>Post2</li>
-                <li>Post3</li>
-                <li>Post4</li>
-                <li>Post1</li>
-                <li>Post2</li>
-                <li>Post3</li>
-                <li>Post4</li>
-                <li>Post1</li>
-                <li>Post2</li>
-                <li>Post3</li>
-                <li>Post4</li>
-                <li>Post1</li>
-                <li>Post2</li>
-                <li>Post3</li>
-                <li>Post4</li>
-                <li>Post1</li>
-                <li>Post2</li>
-                <li>Post3</li>
-                <li>Post4</li>
-                <li>Post1</li>
-                <li>Post2</li>
-                <li>Post3</li>
-                <li>Post4</li>
-                <li>Post1</li>
-                <li>Post2</li>
-                <li>Post3</li>
-                <li>Post4</li>
-                <li>Post1</li>
-                <li>Post2</li>
-                <li>Post3</li>
-                <li>Post4</li>
-                <li>Post1</li>
-                <li>Post2</li>
-                <li>Post3</li>
-                <li>Post4</li>
-                <li>Post1</li>
-                <li>Post2</li>
-                <li>Post3</li>
-                <li>Post4</li>
-                <li>Post1</li>
-                <li>Post2</li>
-                <li>Post3</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
+                <li>{<Posts />}</li>
             </ul>
         </div>
     )}
