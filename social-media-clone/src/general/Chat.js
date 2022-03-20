7bbf63f77e59db8c7165c4f5858dcbafab68441d
adarshakanel <52978035+adarshakanel@users.noreply.github.com>
2022-03-19 18:28:38 -0600

    some css fix

--- social-media-clone/src/general/Chat.js
+++ social-media-clone/src/general/Chat.js
@@ -1,168 +1,101 @@
 import React from 'react'
+import ChatMessages from './ChatMessages';
 
 export const Chat = () => {
-
     return (
-        <div style={{fontFamily: "cursive", color: "#333" }} >
+        <div style={{ fontFamily: "cursive", color: "#333", width: "80%", marginLeft: "10%", height: '100vh' }} >
             <br /><br /><br /><br /><br />
-            <div style={{display: "flex", flexDirection: "row", alignContent: "stretch", justifyItems: "start"}}>
-                <div style={{width: "25%"}}>
-                    <div style={{display: "grid", minHeight: "73vh", maxHeight: "73vh", overflowY: "scroll", border: "1px solid rgba(0,0,20,.33", borderRadius: "5px"}}>
+            <div style={{ display: "flex", flexDirection: "row", alignContent: "stretch", justifyItems: "start" }}>
+                <div style={{ width: "25%" }}>
+                    <div style={{ display: "grid", minHeight: "73vh", maxHeight: "73vh", overflowY: "scroll", border: "1px solid rgba(0,0,20,.33", borderRadius: "5px" }}>
                         <div>
-                            <div style={{padding: "15px", backgroundColor: "#cdcdcd"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", fontWeight: "bold", lineHeight: "24px"}}>
+                            <div style={{ padding: "15px", backgroundColor: "#cdcdcd" }}>
+                                <h4 style={{ marginTop: "10px", marginBottom: "10px", fontSize: "18px", fontWeight: "bold", lineHeight: "24px" }}>
                                     Gordon Ramsay😎😎
                                 </h4>
-                                <h5 style={{marginTop: "10px", marginBottom: "10px", fontSize: "14px",fontWeight: "bold", lineHeight: "20px"}}>
+                                <h5 style={{ marginTop: "10px", marginBottom: "10px", fontSize: "14px", fontWeight: "bold", lineHeight: "20px" }}>
                                     <b>9+ new messages - 3 days ago</b>
                                 </h5>
                             </div>
-                            <div style={{height: "0.25%", background: "rgba(0, 0, 20, .34)"}}>
+                            <div style={{ height: "0.25%", background: "rgba(0, 0, 20, .34)" }}>
                             </div>
                         </div>
                         <div>
-                            <div style={{padding: "15px"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", fontWeight: "bold", lineHeight: "24px"}}>
+                            <div style={{ padding: "15px" }}>
+                                <h4 style={{ marginTop: "10px", marginBottom: "10px", fontSize: "18px", fontWeight: "bold", lineHeight: "24px" }}>
                                     Joe Bastianich👑
                                 </h4>
-                                <h5 style={{marginTop: "10px", marginBottom: "10px", fontSize: "14px",fontWeight: "bold", lineHeight: "20px", color: "rgba(51, 51, 51, .6)"}}>
+                                <h5 style={{ marginTop: "10px", marginBottom: "10px", fontSize: "14px", fontWeight: "bold", lineHeight: "20px", color: "rgba(51, 51, 51, .6)" }}>
                                     Seen
                                 </h5>
                             </div>
-                            <div style={{height: "0.25%", background: "rgba(0, 0, 20, .34)"}}>
+                            <div style={{ height: "0.25%", background: "rgba(0, 0, 20, .34)" }}>
                             </div>
                         </div>
                         <div>
-                            <div style={{padding: "15px"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", fontWeight: "bold", lineHeight: "24px"}}>
+                            <div style={{ padding: "15px" }}>
+                                <h4 style={{ marginTop: "10px", marginBottom: "10px", fontSize: "18px", fontWeight: "bold", lineHeight: "24px" }}>
                                     Graham Elliot
                                 </h4>
-                                <h5 style={{marginTop: "10px", marginBottom: "10px", fontSize: "14px",fontWeight: "bold", lineHeight: "20px"}}>
+                                <h5 style={{ marginTop: "10px", marginBottom: "10px", fontSize: "14px", fontWeight: "bold", lineHeight: "20px" }}>
                                     <b>2+ new messages - 1 day ago</b>
                                 </h5>
                             </div>
-                            <div style={{height: "0.25%", background: "rgba(0, 0, 20, .34)"}}>
+                            <div style={{ height: "0.25%", background: "rgba(0, 0, 20, .34)" }}>
                             </div>
                         </div>
                         <div>
-                            <div style={{padding: "15px"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", fontWeight: "bold", lineHeight: "24px"}}>
+                            <div style={{ padding: "15px" }}>
+                                <h4 style={{ marginTop: "10px", marginBottom: "10px", fontSize: "18px", fontWeight: "bold", lineHeight: "24px" }}>
                                     Luca Manfé 🥇💍✨
                                 </h4>
-                                <h5 style={{marginTop: "10px", marginBottom: "10px", fontSize: "14px",fontWeight: "bold", lineHeight: "20px", color: "rgba(51, 51, 51, .6)"}}>
+                                <h5 style={{ marginTop: "10px", marginBottom: "10px", fontSize: "14px", fontWeight: "bold", lineHeight: "20px", color: "rgba(51, 51, 51, .6)" }}>
                                     Seen
                                 </h5>
                             </div>
-                            <div style={{height: "0.25%", background: "rgba(0, 0, 20, .34)"}}>
+                            <div style={{ height: "0.25%", background: "rgba(0, 0, 20, .34)" }}>
                             </div>
                         </div>
                         <div>
-                            <div style={{padding: "15px"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", fontWeight: "bold", lineHeight: "24px"}}>
+                            <div style={{ padding: "15px" }}>
+                                <h4 style={{ marginTop: "10px", marginBottom: "10px", fontSize: "18px", fontWeight: "bold", lineHeight: "24px" }}>
                                     Christine Hà
                                 </h4>
-                                <h5 style={{marginTop: "10px", marginBottom: "10px", fontSize: "14px",fontWeight: "bold", lineHeight: "20px"}}>
+                                <h5 style={{ marginTop: "10px", marginBottom: "10px", fontSize: "14px", fontWeight: "bold", lineHeight: "20px" }}>
                                     <b>LOL - 2 hours ago</b>
                                 </h5>
                             </div>
-                            <div style={{height: "0.25%", background: "rgba(0, 0, 20, .34)"}}>
+                            <div style={{ height: "0.25%", background: "rgba(0, 0, 20, .34)" }}>
                             </div>
                         </div>
                         <div>
-                            <div style={{padding: "15px"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", fontWeight: "bold", lineHeight: "24px"}}>
+                            <div style={{ padding: "15px" }}>
+                                <h4 style={{ marginTop: "10px", marginBottom: "10px", fontSize: "18px", fontWeight: "bold", lineHeight: "24px" }}>
                                     Courtney Lapresi
                                 </h4>
-                                <h5 style={{marginTop: "10px", marginBottom: "10px", fontSize: "14px",fontWeight: "bold", lineHeight: "20px", color: "rgba(51, 51, 51, .6)"}}>
+                                <h5 style={{ marginTop: "10px", marginBottom: "10px", fontSize: "14px", fontWeight: "bold", lineHeight: "20px", color: "rgba(51, 51, 51, .6)" }}>
                                     Seen
                                 </h5>
                             </div>
-                            <div style={{height: "0.25%", background: "rgba(0, 0, 20, .34)"}}>
+                            <div style={{ height: "0.25%", background: "rgba(0, 0, 20, .34)" }}>
                             </div>
                         </div>
                         <div>
-                            <div style={{padding: "15px"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", fontWeight: "bold", lineHeight: "24px"}}>
+                            <div style={{ padding: "15px" }}>
+                                <h4 style={{ marginTop: "10px", marginBottom: "10px", fontSize: "18px", fontWeight: "bold", lineHeight: "24px" }}>
                                 </h4>
-                                <h5 style={{marginTop: "10px", marginBottom: "10px", fontSize: "14px",fontWeight: "bold", lineHeight: "20px", color: "rgba(51, 51, 51, .6)"}}>
+                                <h5 style={{ marginTop: "10px", marginBottom: "10px", fontSize: "14px", fontWeight: "bold", lineHeight: "20px", color: "rgba(51, 51, 51, .6)" }}>
                                 </h5>
                             </div>
-                            <div style={{height: "0.25%", background: "rgba(0, 0, 20, .34)"}}>
+                            <div style={{ height: "0.25%", background: "rgba(0, 0, 20, .34)" }}>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div style={{width: "0.25%", background: "rgba(51,51,51,.37)"}}></div>
-                <div style={{width: "75%", borderBottom: "1px solid rgba(0,0,0,.24)", borderTop: "1px solid rgba(0,0,0,.24)", borderRadius: "1px", padding: "29px", paddingBottom: "0px", minHeight: "73vh", maxHeight: "73vh"}}>
-                    <h1 style={{paddingBottom: "15px", fontSize: "38px", lineHeight: "44px", fontWeight: "bold", textAlign: "center"}}>
-                        Gordon Ramsay😎😎
-                    </h1>
-                    <div style={{display: "flex", flexDirection: "column", alignContent:"stretch", justifyItems: "end", border: "1px solid rgba(0,0,20,.33", borderRadius: "5px"}}>
-                        <div style={{display: "grid", paddingBottom: "10px", paddingTop:"10px"}}>
-                            <div style={{justifySelf: "end", backgroundColor: "#4460ff", paddingRight: "20px", paddingLeft: "20px", marginTop: "5px",marginRight: "20px", borderTopLeftRadius: "20px", borderTopRightRadius: "0px", borderBottomLeftRadius: "20px", borderBottomRightRadius: "20px"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", lineHeight: "24px", color: "white"}}>
-                                    HI
-                                </h4>
-                            </div>
-                            <div style={{justifySelf: "start", backgroundColor: "darkgrey", paddingRight: "20px", paddingLeft: "20px", marginTop: "5px", marginLeft: "20px", borderTopLeftRadius: "0px", borderTopRightRadius: "20px", borderBottomLeftRadius: "20px", borderBottomRightRadius: "20px"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", lineHeight: "24px", color: "white"}}>
-                                    Hello
-                                </h4>
-                            </div>
-                            <div style={{justifySelf: "end", backgroundColor: "#4460ff", paddingRight: "20px", paddingLeft: "20px", marginTop: "5px", marginRight: "20px", borderTopLeftRadius: "20px", borderTopRightRadius: "0px", borderBottomLeftRadius: "20px", borderBottomRightRadius: "20px"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", lineHeight: "24px", color: "white"}}>
-                                    Goodbye
-                                </h4>
-                            </div>
-                            <div style={{justifySelf: "start", backgroundColor: "darkgrey", paddingRight: "20px", paddingLeft: "20px", marginTop: "5px", marginLeft: "20px", borderTopLeftRadius: "0px", borderTopRightRadius: "20px", borderBottomLeftRadius: "20px", borderBottomRightRadius: "20px"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", lineHeight: "24px", color: "white"}}>
-                                bruh-
-                                </h4>
-                            </div>
-                            <div style={{justifySelf: "end", backgroundColor: "#4460ff", paddingRight: "20px", paddingLeft: "20px", marginTop: "5px", marginRight: "20px", borderTopLeftRadius: "20px", borderTopRightRadius: "0px", borderBottomLeftRadius: "20px", borderBottomRightRadius: "20px"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", lineHeight: "24px", color: "white"}}>
-                                i said goodbye.
-                                </h4>
-                            </div>
-                            <div style={{justifySelf: "start", backgroundColor: "darkgrey", paddingRight: "20px", paddingLeft: "20px", marginTop: "5px", marginLeft: "20px", borderTopLeftRadius: "0px", borderTopRightRadius: "20px", borderBottomLeftRadius: "20px", borderBottomRightRadius: "20px"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", lineHeight: "24px", color: "white"}}>
-                                Nvm
-                                </h4>
-                            </div>
-                            <div style={{justifySelf: "end", backgroundColor: "#4460ff", paddingRight: "20px", paddingLeft: "20px", marginTop: "5px", marginRight: "20px", borderTopLeftRadius: "20px", borderTopRightRadius: "0px", borderBottomLeftRadius: "20px", borderBottomRightRadius: "20px"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", lineHeight: "24px", color: "white"}}>
-                                Hello Again!
-                                </h4>
-                            </div>
-                            <div style={{justifySelf: "start", backgroundColor: "darkgrey", paddingRight: "20px", paddingLeft: "20px", marginTop: "5px", marginLeft: "20px", borderTopLeftRadius: "0px", borderTopRightRadius: "20px", borderBottomLeftRadius: "20px", borderBottomRightRadius: "20px"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", lineHeight: "24px", color: "white"}}>
-                                Nah, goodbye-
-                                </h4>
-                            </div>
-                            <div style={{justifySelf: "end", backgroundColor: "#4460ff", paddingRight: "20px", paddingLeft: "20px", marginTop: "5px", marginRight: "20px", borderTopLeftRadius: "20px", borderTopRightRadius: "0px", borderBottomLeftRadius: "20px", borderBottomRightRadius: "20px"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", lineHeight: "24px", color: "white"}}>
-                                    Goodbye
-                                </h4>
-                            </div>
-                            <div style={{justifySelf: "end", backgroundColor: "#4460ff", paddingRight: "20px", paddingLeft: "20px", marginTop: "5px", marginRight: "20px", borderTopLeftRadius: "20px", borderTopRightRadius: "0px", borderBottomLeftRadius: "20px", borderBottomRightRadius: "20px"}}>
-                                <h4 style={{marginTop: "10px", marginBottom: "10px", fontSize: "18px", lineHeight: "24px", color: "white"}}>
-                                    REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-                                </h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div style={{display:'flex', flexDirection:"row", alignContent: "center", justifyItems: "space-between",paddingTop: "15px"}}>
-                        <form style={{display:'flex', flexDirection:"row", width: "100%", alignItems: "center"}}>
-                            <label style={{width: "100%", paddingTop: "auto"}}>
-                                <input style={{width: "100%" , border: "1px solid rgba(0,0,20,.33", borderRadius: "15px"}} type="text" name="name" placeholder='  Message' />
-                            </label>
-                            <h4 style={{paddingLeft: "15px", paddingRight: "15px"}}> + </h4>
-                            <input style={{paddingLeft: "10px", paddingRight: "10px",paddingTop:"auto", paddingBottom:"auto", border: "1px solid rgba(0,0,20,.33", borderRadius: "5px"}} type="submit" value="Send" />
-                        </form>
-                    </div>
-                </div>
+                <div style={{ width: "0.25%", background: "rgba(51,51,51,.37)" }}></div>
+                <ChatMessages />
             </div>
-        </div>
+        </div >
     )
 }
 
