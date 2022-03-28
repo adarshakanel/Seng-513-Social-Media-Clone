8ef8108909658e8ad3e119a04295dfd042744019
PriyavartRajain <47432873+PriyavartRajain@users.noreply.github.com>
2022-03-27 19:12:40 -0600

    send signup form data to backend

--- social-media-clone/src/general/SignUp.js
+++ social-media-clone/src/general/SignUp.js
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useState, useContext, useRef } from 'react'
 import logo from '../Resources/camera.png'
 import '../css/SignUp.css';
 import { useNavigate } from 'react-router-dom'
+import AppContext from '../context/AppContext'
 
 export const SignUp = () => {
+
+    const formVal = {
+        email: '',
+        fullname: '',
+        username: '',
+        password: ''
+    }
+
+    const { url } = useContext(AppContext)
+    const [formValues, setFormValues] = useState(formVal)
+    const email = useRef();
+    const fullname = useRef();
+    const username = useRef();
+    const password = useRef();
+
+    const [signUpSuccessful, setSignUpSuccessful] = useState(false);
+
     const navigate = useNavigate();
 
     function gotoLogin(e) {
         e.preventDefault()
         navigate('/login', { replace: true })
     }
+
+    function createUser(e) {
+        let formVals = { email: email.current.value, fullName: fullname.current.value, username: username.current.value, password: password.current.value };
+        e.preventDefault()
+        fetch(url + 'signup', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formVals)
+        }).then(response => response.ok ? setSignUpSuccessful(true) && setFormValues(formVals) : null)
+        console.log(formVals);
+    }
+
     return (
         <div className="background-div">
             <div className='popup signup-content'>
@@ -20,12 +50,12 @@ export const SignUp = () => {
 
                 <form className='sign-up-login-form'>
                     <div className='form-fields'>
-                        <input type="email" placeholder="Email" className='form-field'></input>
-                        <input type="text" placeholder="Full Name" className='form-field'></input>
-                        <input type="text" placeholder="Username" className='form-field'></input>
-                        <input type="password" placeholder="Password" className='form-field'></input>
+                        <input type="email" placeholder="Email" className='form-field' ref={email}></input>
+                        <input type="text" placeholder="Full Name" className='form-field' ref={fullname}></input>
+                        <input type="text" placeholder="Username" className='form-field' ref={username}></input>
+                        <input type="password" placeholder="Password" className='form-field' ref={password}></input>
                     </div>
-                    <button className='submit-btn'>Sign Up</button>
+                    <button className='submit-btn' onClick={e => createUser(e)}>Sign Up</button>
                 </form>
                 <div className='offer-login-signup'>
                     <p>Have an account?
